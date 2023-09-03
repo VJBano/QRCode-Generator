5e31dbbbf36d63a279c2eb911994682e5ccc0f90
vincent <vincentjaybano45@gmail.com>
2023-09-03 13:15:34 +0800

    setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/QRCode_Generator/",
+  base:"/QRCode-Generator/",
   server:{
     host:true,
     strictPort:false,
